007b7c06a0288015b0f02bee402c1dd2b6aef3db
Pavlo Berezin <pavlo.berezin@gmail.com>
2017-05-24 19:10:23 +0300

    Added search bar

--- src/containers/ArticlesList.js
+++ src/containers/ArticlesList.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react'
 import Article from '../components/Article'
+import SearchList from '../containers/SearchList'
 import { Link } from 'react-router-dom'
 export default class ArticlesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {articles: []};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {articles: []};
+    }
 
-  loadData() {
-    fetch('article')
-      .then(response => response.json())
-      .then(response => {
-        this.setState({
-          articles: response.articles
+    loadData() {
+        fetch('article')
+        .then(response => response.json())
+        .then(response => {
+            this.setState({
+                articles: response.articles
+            });
         });
-      })
-  }
+    }
 
-  componentWillMount() {
-    this.loadData();
-  }
+    componentWillMount() {
+        this.loadData();
+    }
 
   render() {
       return (
-        <div className="articles-list">
-            {this.state.articles.map((article, index) =>
-                <div key={article._id} className="article-container">
-                    <Article {...article} id={article._id}/>
-                    <Link to={'/article/' + article._id} key={index} className="more-link">Go to article</Link>
+            <div className="articles-list-container">
+                <div className="search-list-container">
+                    <SearchList/>
                 </div>
-            )}
-        </div>
-      )
-  }
+                <div className="articles-list">
+                    {this.state.articles.map((article, index) =>
+                        <div key={article._id} className="article-container">
+                            <Article {...article} id={article._id}/>
+                            <Link to={'/article/' + article._id} key={index} className="more-link">Go to article</Link>
+                        </div>
+                    )}
+                </div>
+            </div>
+        )
+    }
 }
---
+++ src/containers/SearchList.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react'
+import Badge from '../components/Badge'
+export default class SearchList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [],
+            newBadge: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    addNewBadge() {
+        let categories = this.state.categories;
+        categories.push(this.state.newBadge);
+        this.setState({
+            categories: categories,
+            newBadge: ''
+        });
+    }
+
+    handleChange(event) {
+        let obj = {};
+        let target = event.target;
+        obj[target.name] = target.value;
+        this.setState(Object.assign({}, this.state, obj));
+    }
+
+    render() {
+        return (
+            <div className="search-list">
+                <div className="card-title">
+                    <span>Search by category</span>
+                </div>
+                <div className="badges-list">
+                    {this.state.categories.map((category, index) =>
+                        <Badge name={category} key={index} ></Badge>
+                    )}
+                </div>
+                <div className="add-new">
+                    <input name="newBadge" value={this.state.newBadge} onChange={this.handleChange}></input>
+                    <button onClick={() => this.addNewBadge()}>+</button>
+                </div>
+            </div>
+      )
+    }
+}
