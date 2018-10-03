bb0abc39489c09ff3d84a1a7ea598a8ebc711081
Pavlo Berezin <14924742+pavlo-berezin@users.noreply.github.com>
2018-10-04 00:50:46 +0300

    Added support for redux devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "../reducers";
 import thunk from 'redux-thunk';
 
-export const getStore = () => createStore(rootReducer, applyMiddleware(thunk));
+const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const getStore = () => createStore(rootReducer,
+                                          devToolsExtension,
+                                          applyMiddleware(thunk));
