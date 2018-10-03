7b26ea127e7eba94d25759c8049b08e0e1fe4d31
Pavlo Berezin <14924742+pavlo-berezin@users.noreply.github.com>
2018-10-03 12:56:49 +0300

    test commit

--- src/containers/ArticlesList.js
+++ src/containers/ArticlesList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Article from '../components/Article'
 import SearchList from '../containers/SearchList'
 import { Link } from 'react-router-dom'
+
 export default class ArticlesList extends Component {
     constructor(props) {
         super(props);
