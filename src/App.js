ee7e600ba4216b66f7ed5448f60a753ef27b0b96
Pavlo Berezin <14924742+pavlo-berezin@users.noreply.github.com>
2018-10-04 13:46:20 +0300

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import './App.scss'
-import ArticlesList from './containers/ArticlesList'
-import ArticleView from './containers/ArticleView'
-import CreateArticle from './containers/CreateArticle'
-import LoginPage from './containers/LoginPage'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import './App.scss';
+import ArticlesList from './containers/ArticlesList';
+import ArticleView from './containers/ArticleView';
+import CreateArticle from './containers/CreateArticle';
+import LoginPage from './containers/LoginPage';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        let username = localStorage.getItem('username');
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: username
-        }
+    this.state = {
+      username: localStorage.getItem('username')
     }
+  }
 
-    requireAuth() {
-        if (!this.state.username) {
-            console.log('inside if');
-            console.log(this.props);
-        }
+  requireAuth() {
+    if (!this.state.username) {
+      console.log('inside if');
+      console.log(this.props);
     }
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <div className="App-header">
-                        <div className="title">Text Categorizer</div>
-                        <ul className="menu">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/create">Create</Link></li>
-                        </ul>
-                        <div className="username">
-                            {!this.state.username && <Link to="/login">Login</Link>}
-                            {this.state.username && <span>{this.state.username}</span>}
-                        </div>
-                    </div>
-                   <div className="App-body">
-                        <Route exact path="/" component={ArticlesList}/>
-                        <Route path="/login" component={LoginPage}/>
-                        <Route path="/create" component={CreateArticle}/>
-                        <Route path="/article/:id" component={ArticleView}/>
-                    </div>
-                </div>
-            </Router>
-        );
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <div className="title">Text Categorizer</div>
+            <ul className="menu">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/create">Create</Link></li>
+            </ul>
+            <div className="username">
+              {!this.state.username && <Link to="/login">Login</Link>}
+              {this.state.username && <span>{this.state.username}</span>}
+            </div>
+          </div>
+          <div className="App-body">
+            <Route exact path="/" component={ArticlesList} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/create" component={CreateArticle} />
+            <Route path="/article/:id" component={ArticleView} />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
